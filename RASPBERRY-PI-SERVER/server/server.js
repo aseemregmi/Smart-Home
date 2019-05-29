19e9cc8008eb1c62328d5ec1c1d6813629f5995f
Aseem Regmi <aseem.regmi22@gmail.com>
2019-05-29 06:17:02 +0100

    Added rpio module

--- RASPBERRY-PI-SERVER/server/server.js
+++ RASPBERRY-PI-SERVER/server/server.js
@@ -1,16 +1,23 @@
 const app = require('express')();
+const rpio = require('rpio');
 
 const port = process.env.PORT || 3000;
+rpio.open(7, rpio.OUTPUT, rpio.LOW);
 
 app.post('/on', (req, res) => {
+console.log('Request to on received');
   res.send('PIN On');
+        rpio.write(7, rpio.HIGH);
 });
 
 app.post('/off', (req, res) => {
+console.log('Request to off received');
+        rpio.write(7, rpio.LOW);
   res.send('PIN Off');
 });
 
 app.get('/', (req, res) => {
+	res.send(rpio.read(7));
   // Fetch Current Status
 });
 

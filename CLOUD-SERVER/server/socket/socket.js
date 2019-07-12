1a08ad29cb7ce97931c77bed7efd428c245b1688
Namit Adhikari <official.namit321@gmail.com>
2019-07-12 18:12:16 +0545

    Updates in PI

--- CLOUD-SERVER/server/socket/socket.js
+++ CLOUD-SERVER/server/socket/socket.js
@@ -1,12 +1,12 @@
 const socket = require('socket.io');
+const pool = require('./../../database/db');
 let io = null;
-socketId = '';
 
-const on = data => {
+const on = (socketId, data) => {
   io.to(socketId).emit('on', data);
 };
 
-const off = data => {
+const off = (socketId, data) => {
   io.to(socketId).emit('off', data);
 };
 
@@ -15,13 +15,36 @@ const socketInitFunction = server => {
   io = socketServer;
 
   socketServer.on('connection', socket => {
-    socket.on('init', data => {
-      // Need to store socket id with corresponding rpi_id in db
-      // For now just considering a single pi
-      socketId = socket.id;
+    // socket.on('init', async data => {
+    //   query = //   UPDATE raspberry_pi
+    //     SET socket_id='${socket.id}'
+    //     WHERE rpi_id='${data.rpi_id}'
+    //;
 
-      // If available get state from cache and emit to the pi
+    //   await pool.query(query);
+    //   // If available get state from cache and emit to the pi
+
+    // });
+
+    socket.emit('scheduleWithEvent', {
+      gpio: 4,
+      time: new Date(Date.now() + 60 * 1000),
+      action: true
     });
+
+    socket.emit('scheduleWithTime', {
+      gpio: 18,
+      startTime: '23:20',
+      endTime: '23:25'
+    });
+
+    setTimeout(() => {
+      socket.emit('scheduleWithTime', {
+        gpio: 4,
+        startTime: new Date(Date.now() + 60 * 1000),
+        endTime: new Date(Date.now() + 120 * 1000)
+      });
+    }, 5000);
 
     socket.on('disconnect', () => {
       // Close all sessions but cache the state
--- RASPBERRY-PI-SERVER/pi.py
+++ RASPBERRY-PI-SERVER/pi.py
@@ -1,20 +1,49 @@
 import socketio
-from gpiozero import LED
+from gpiozero import LED, LEDBoard
 import time
 
 sio = socketio.Client()
+leds = LEDBoard(18, 4, 16)
+
+rpi_id = 1
 
 @sio.on('connect')
 def on_connect():
-    print('I\'m connected')
+    sio.emit('init', {'rpi_id': rpi_id})
+    print("Connected")
 
-@sio.on('hi')
+@sio.on('on')
+def on_on(data):
+    print("Handle On")
+    print(data)
+    
+    led = LED(data['gpio'])
+    led.on()
+
+
+@sio.on('off')
+def on_off(data):
+    print("Handle off")
+    print(data)
+
+    led = LED(data['gpio'])
+    led.off()
+
+
+@sio.on('schedule')
 def on_message(data):
-    led = LED(data['pin'])
-    led.on()  
+
+        led = LED(data['gpio'])
+        
+        while True:
+                if (int(time.time()) == data['datetime'] ):
+                        if (data['action']):
+                                led.on()
+                        else:
+                                led.off()
 
 @sio.on('disconnect')
 def on_disconnect():
     print('I\'m disconnected')
 
-sio.connect('http://localhost:3000/')
+sio.connect('http://192.168.1.8:3000/')
